d7da4681d5a6d846a890f3817bc669017c2a342d
Sujal Patel <spatel2@madisoncollege.edu>
2023-04-08 14:10:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useReducer} from 'react'
+import { API } from 'aws-amplify'
+import { List } from 'antd'
+import 'antd/dist/antd.css'
+import { listNotes } from './graphql/queries'
 
-function App() {
+const initialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: { name: '', description: '' }
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'SET_NOTES':
+      return { ...state, notes: action.notes, loading: false }
+    case 'ERROR':
+      return { ...state, loading: false, error: true }
+    default:
+      return {...state};
+  }
+};
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
 
